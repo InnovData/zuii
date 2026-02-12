772ae0fba49dfa36c1bc1218594aac1f9d318a4f
Vincent <50195411+vincentm498@users.noreply.github.com>
2026-02-12 16:16:58 +0100

    Ajoute le composant Alerte avec sa logique JS, ses styles et son intégration SweetAlert2, et met à jour les formulaires et les jetons de bordure.

--- src/templates/Forms/Forms-elements.tsx
+++ src/templates/Forms/Forms-elements.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import { Form } from '../../index';
 
 /**
@@ -6,45 +6,58 @@ import { Form } from '../../index';
  * @returns {JSX.Element} La page de démo des forms.
  */
 export const FormsElements = () => {
+	const [value, setValue] = useState<string | string[]>(['1']);
+
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const formData = new FormData(e.currentTarget);
+		console.log(Object.fromEntries(formData.entries()));
+		console.log(formData.getAll('selectMultiple[]'));
+	}
 
 	return (
 		<div>
 			<h2>Liste des inputs disponibles</h2>
-			<Form.Control type="text" placeholder="Enter text"  />
-			<Form.Control type="search" placeholder="Enter search"  />
-			<Form.Control type="email" placeholder="Enter email"  />
-			<Form.Control type="number" placeholder="Enter number"  />
-			<Form.Control type="file" placeholder="Enter file"  />
-			<Form.Control type="checkbox" placeholder="Enter checkbox"  />
-			<Form.Control type="radio" placeholder="Enter radio"  />
-			<Form.Select options={[
-				{
-					text: 'Option 1',
-					value: '1'
-				},
-				{
-					text: 'Option 2',
-					value: '2'
-				},
-				{
-					text: 'Option 3',
-					value: '3'
-				}
-			]} />
-			<Form.Select multiple={true} options={[
-				{
-					text: 'Option 1',
-					value: '1'
-				},
-				{
-					text: 'Option 2',
-					value: '2'
-				},
-				{
-					text: 'Option 3',
-					value: '3'
-				}
-			]} />
+
+			<form action="" onSubmit={handleSubmit}>
+				<Form.Control type="text" placeholder="Enter text" name="text" />
+				<Form.Control type="search" placeholder="Enter search" name="search" />
+				<Form.Control type="email" placeholder="Enter email" name="email" />
+				<Form.Control type="number" placeholder="Enter number" name="number" />
+				<Form.Control type="file" placeholder="Enter file" name="file" />
+				<Form.Control type="checkbox" placeholder="Enter checkbox" name="checkbox" />
+				<Form.Control type="radio" placeholder="Enter radio" name="radio" />
+				<Form.Select name="select" value={value} onChange={(value) => setValue(value)} options={[
+					{
+						text: 'Option 1',
+						value: '1'
+					},
+					{
+						text: 'Option 2',
+						value: '2'
+					},
+					{
+						text: 'Option 3',
+						value: '3'
+					}
+				]} />
+				<Form.Select name="selectMultiple[]" value={['1', '2']} multiple={true} options={[
+					{
+						text: 'Option 1',
+						value: '1'
+					},
+					{
+						text: 'Option 2',
+						value: '2'
+					},
+					{
+						text: 'Option 3',
+						value: '3'
+					}
+				]} />
+				<Form.Control type="submit" value="Submit" />
+			</form>
 
 		</div>
 	);
